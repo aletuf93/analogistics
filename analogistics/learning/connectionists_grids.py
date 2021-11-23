d9d512add239b3c03d06722afbc3b9903714a5d1
aletuf93 <49878838+aletuf93@users.noreply.github.com>
2021-11-23 23:38:10 +0100

    OK learning methods

--- analogistics/learning/connectionists_grids.py
+++ analogistics/learning/connectionists_grids.py
@@ -1,31 +1,27 @@
 # %% CLASSIFICATION MODEL PARAMETERS
 from sklearn.neural_network import MLPClassifier, MLPRegressor
 
-tuned_param_perc= [{
-                        'hidden_layer_sizes':[1,2,10,50,100],
-                        'activation':['identity','logistic','tanh','relu'],
-                        'learning_rate':['constant','invscaling','adaptive']
-                  }]
-                                
-models_classification = {
-                            'perceptron single layer': {
-                                   'estimator': MLPClassifier(), 
-                                   'param': tuned_param_perc,
-                            },
-                        }
+from analogistics.learning.grids import GridSearch
 
-# %% REGRESSION MODEL PARAMETERS
+tuned_param_perc = [{'hidden_layer_sizes': [1, 2, 10, 50, 100],
+                     'activation': ['identity', 'logistic', 'tanh', 'relu'],
+                     'learning_rate':['constant', 'invscaling', 'adaptive']
+                     }]
 
-tuned_param_perc_regr= [{
-                        'hidden_layer_sizes':[1,2,10,50,100],
-                        'activation':['identity','logistic','tanh','relu'],
-                        'learning_rate':['constant','invscaling','adaptive']
-                  }]
-                                
-models_regression = {
-                            'perceptron single layer': {
-                                   'estimator': MLPRegressor(), 
-                                   'param': tuned_param_perc_regr,
-                            },
-                        }
+tuned_param_perc_regr = [{'hidden_layer_sizes': [1, 2, 10, 50, 100],
+                          'activation': ['identity', 'logistic', 'tanh', 'relu'],
+                          'learning_rate': ['constant', 'invscaling', 'adaptive']
+                          }]
 
+
+class GridSearchConnectionist(GridSearch):
+    def __init__(self):
+        self.models_regression = {'perceptron single layer': {'estimator': MLPRegressor(),
+                                                              'param': tuned_param_perc_regr,
+                                                              },
+                                  }
+
+        self.models_classification = {'perceptron single layer': {'estimator': MLPClassifier(),
+                                                                  'param': tuned_param_perc
+                                                                  },
+                                      }
--- analogistics/learning/ensemble_grids.py
+++ analogistics/learning/ensemble_grids.py
@@ -1,99 +1,70 @@
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, BaggingClassifier
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor, BaggingRegressor
-# %% CLASSIFICATION MODEL PARAMETERS
 
-tuned_param_rf= [{'n_estimators':[10,50,100,200],
-                  #'criterion':['gini','entropy'],
-                  'max_features':['auto','sqrt','log2'],
-                  'max_depth': range(1,20)                 
-                  }]
+from analogistics.learning.grids import GridSearch
 
+tuned_param_rf = [{'n_estimators': [10, 50, 100, 200],
+                   # 'criterion':['gini','entropy'],
+                   'max_features': ['auto', 'sqrt', 'log2'],
+                   'max_depth': range(1, 20)
+                   }]
 
-tuned_param_ab = [{'n_estimators':[1, 10, 50, 100]               
-                  }]
+tuned_param_ab = [{'n_estimators': [1, 10, 50, 100]
+                   }]
 
+tuned_param_gb = [{'loss': ['deviance', 'exponential'],
+                   'learning_rate':[1e-2, 1e-1, 1, 10],
+                   'n_estimators':[1, 2, 10, 50, 100]
+                   }]
 
-tuned_param_gb =  [{'loss':['deviance','exponential'],
-                    'learning_rate':[1e-2, 1e-1, 1, 10],
-                    'n_estimators':[1,2,10,50,100]
-                  
-                              
-                  }]
-
-tuned_param_bt = [{'n_estimators':[1, 10, 50, 100]               
-                         }]
-                                
-models_classification = {
-                            
-                            
-                            'random forest': {
-                                   'estimator': RandomForestClassifier(), 
-                                   'param': tuned_param_rf,
-                            },
-                            
-                            'adaboost': {
-                                   'estimator': AdaBoostClassifier(), 
-                                   'param': tuned_param_ab,
-                            },
-                            
-                            'gradient boosting': {
-                                   'estimator': GradientBoostingClassifier(), 
-                                   'param': tuned_param_gb,
-                            },
-                            
-                            'bagging tree': {
-                                   'estimator': BaggingClassifier(), 
-                                   'param': tuned_param_bt,
-                            },
-                        }
-
-# %% REGRESSION MODEL PARAMETERS
-
-tuned_param_rf_regr= [{'n_estimators':[10,50,100,200],
-                            #'criterion':['gini','entropy'],
-                          'max_features':['auto','sqrt','log2'],
-                          'max_depth': range(1,20)                 
-                          }]
-
-tuned_param_ab_regr  = [{'n_estimators':[1, 10, 50, 100]               
-                         }]
-
-tuned_param_gb_regr = [{'loss':['ls','lad','huber','quantile'],
-                    'learning_rate':[1e-2, 1e-1, 1, 10],
-                    'n_estimators':[1,2,10,50,100]
-                  
-                              
-                      }]
-
-tuned_param_bt_regr = [{'n_estimators':[1, 10, 50, 100]               
-                         }]
+tuned_param_bt = [{'n_estimators': [1, 10, 50, 100]
+                   }]
 
 
-models_regression = {
-                            
-                            
-                            'random forest': {
-                                   'estimator': RandomForestRegressor(), 
-                                   'param': tuned_param_rf_regr,
-                            },
-                            
-                            'adaboost': {
-                                   'estimator': AdaBoostRegressor(), 
-                                   'param': tuned_param_ab_regr,
-                            },
-                            
-                            'gradient boosting': {
-                                   'estimator': GradientBoostingRegressor(), 
-                                   'param': tuned_param_gb_regr,
-                            },
-                            
-                            'bagging tree': {
-                                   'estimator': BaggingRegressor(), 
-                                   'param': tuned_param_bt_regr,
-                            },
-                            
-                            
-                        }
+tuned_param_rf_regr = [{'n_estimators': [10, 50, 100, 200],
+                        # 'criterion':['gini','entropy'],
+                        'max_features': ['auto', 'sqrt', 'log2'],
+                        'max_depth': range(1, 20)
+                        }]
+
+tuned_param_ab_regr = [{'n_estimators': [1, 10, 50, 100]
+                        }]
+
+tuned_param_gb_regr = [{'loss': ['ls', 'lad', 'huber', 'quantile'],
+                        'learning_rate': [1e-2, 1e-1, 1, 10],
+                        'n_estimators': [1, 2, 10, 50, 100]
+                        }]
+
+tuned_param_bt_regr = [{'n_estimators': [1, 10, 50, 100]
+                        }]
 
 
+class GridSearchConnectionist(GridSearch):
+    def __init__(self):
+        self.models_regression = {'random forest': {'estimator': RandomForestRegressor(),
+                                                    'param': tuned_param_rf_regr,
+                                                    },
+                                  'adaboost': {'estimator': AdaBoostRegressor(),
+                                               'param': tuned_param_ab_regr,
+                                               },
+                                  'gradient boosting': {'estimator': GradientBoostingRegressor(),
+                                                        'param': tuned_param_gb_regr,
+                                                        },
+                                  'bagging tree': {'estimator': BaggingRegressor(),
+                                                   'param': tuned_param_bt_regr,
+                                                   },
+                                  }
 
+        self.models_classification = {'random forest': {'estimator': RandomForestClassifier(),
+                                                        'param': tuned_param_rf,
+                                                        },
+                                      'adaboost': {'estimator': AdaBoostClassifier(),
+                                                   'param': tuned_param_ab,
+                                                   },
+                                      'gradient boosting': {'estimator': GradientBoostingClassifier(),
+                                                            'param': tuned_param_gb,
+                                                            },
+                                      'bagging tree': {'estimator': BaggingClassifier(),
+                                                       'param': tuned_param_bt,
+                                                       },
+                                      }
