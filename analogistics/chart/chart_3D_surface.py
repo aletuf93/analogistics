3a77fab150f88393f6e857db08888da164b6b156
aletuf93 <49878838+aletuf93@users.noreply.github.com>
2021-12-04 16:31:54 +0100

    Updated structure for graphs

--- analogistics/chart/chart_3D_surface.py
+++ analogistics/chart/chart_3D_surface.py
@@ -1,5 +1,41 @@
 import plotly.graph_objects as go
+import pandas as pd
 import numpy as np
+from scipy.interpolate import griddata
+
+
+def surfaceFromPoints(D_input: pd.DataFrame, xCol: str, yCol: str, zCol: str):
+    """
+    represent a 3D function from points
+
+    Args:
+        D_input (pd.DataFrame): Input pandas dataframe.
+        xCol (str): name of the column with x coordinates.
+        yCol (str): name of the column with y coordinates.
+        zCol (str): name of the column with z coordinates.
+
+    Returns:
+        X (TYPE): DESCRIPTION.
+        Y (TYPE): DESCRIPTION.
+        grid (TYPE): DESCRIPTION.
+
+    """
+    # identify the rectangular to represent
+    min_x = min(D_input[xCol])
+    max_x = max(D_input[xCol])
+
+    min_y = min(D_input[yCol])
+    max_y = max(D_input[yCol])
+
+    # define the grid
+    x = np.linspace(min_x, max_x, 100)
+    y = np.linspace(min_y, max_y, 100)
+    X, Y = np.meshgrid(x, y)
+    xy_coord = list(zip(D_input[xCol], D_input[yCol]))
+
+    # interpolate the function in the missing points
+    grid = griddata(xy_coord, np.array(D_input[zCol]), (X, Y), method='linear')
+    return X, Y, grid
 
 
 def createsurfaceFromGrid(X: np.array, Y: np.array, grid: np.array) -> go.Figure:
