90e79aa968ae6bbd0696db886360db499cf5a660
aletuf93 <49878838+aletuf93@users.noreply.github.com>
2021-12-04 18:43:09 +0100

    Create __init__.py

---
+++ analogistics/chart/__init__.py
@@ -0,0 +1,22 @@
+# %% Import figures from mtplotLib
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+
+
+def fig_to_uri(in_fig, close_all=True, **save_args):
+    # type: (plt.Figure) -> str
+    """
+    Save a figure as a URI
+    :param in_fig:
+    :return:
+    """
+    out_img = BytesIO()
+    in_fig.savefig(out_img, format='png', **save_args)
+    if close_all:
+        in_fig.clf()
+        plt.close('all')
+    out_img.seek(0)  # rewind file
+    encoded = base64.b64encode(out_img.read()).decode("ascii").replace("\n", "")
+    return "data:image/png;base64,{}".format(encoded)
